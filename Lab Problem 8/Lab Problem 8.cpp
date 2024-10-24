b1b3c17a4f60632e4e7699a9f5b4b145652a9b2e
Grady Richwalsky <g-richwalsky@onu.edu>
2024-10-24 12:40:15 -0400

    Completed countCharacter function

--- Lab Problem 8/Lab Problem 8.cpp
+++ Lab Problem 8/Lab Problem 8.cpp
@@ -2,8 +2,22 @@
 //
 
 #include <iostream>
+#include <string>
+#include <iomanip>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int countCharacter(string str);
 
 int main()
 {
     //Testing
+    cout << countCharacter("ACBDEF");
+}
+
+int countCharacter(string str) {
+    int strLen = str.length();
+    return strLen;
 }
