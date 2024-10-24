4547a482bbec35dd0c93ca8827f5121b05209d6e
Grady Richwalsky <g-richwalsky@onu.edu>
2024-10-24 12:51:29 -0400

    Completed countVowel function

--- Lab Problem 8/Lab Problem 8.cpp
+++ Lab Problem 8/Lab Problem 8.cpp
@@ -9,15 +9,47 @@
 
 using namespace std;
 
-int countCharacter(string str);
+int countVowel(string str);
 
 int main()
 {
     //Testing
-    cout << countCharacter("ACBDEF");
+
+    string input;
+
+    cout << "Enter a string or Q to quit: ";
+    cin >> input;
+    cout << "Vowel Count: " << countVowel(input) << endl;
 }
 
-int countCharacter(string str) {
-    int strLen = str.length();
-    return strLen;
+int countVowel(string str) {
+    int strLen = str.size();
+    int numVow = 0;
+    char currentLetter;
+    for (int i = 0; i < strLen; i++) {
+        currentLetter = str[i];
+        switch (currentLetter) {
+        case 'A': numVow++;
+            break;
+        case 'a': numVow++;
+            break;
+        case 'E': numVow++;
+            break;
+        case 'e': numVow++;
+            break;
+        case 'I': numVow++;
+            break;
+        case 'i': numVow++;
+            break;
+        case 'O': numVow++;
+            break;
+        case 'o': numVow++;
+            break;
+        case 'U': numVow++;
+            break;
+        case 'u': numVow++;
+            break;
+        }
+    }
+    return numVow;
 }
